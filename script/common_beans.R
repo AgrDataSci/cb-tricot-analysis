8f823efe28fb58dd0171d9f2075338c51f8a250c
kauedesousa <kaue.desousa@inn.no>
2020-09-17 11:32:00 +0200

    add-data

---
+++ script/common_beans.R
@@ -0,0 +1,49 @@
+library("climatrends")
+library("tidyverse")
+library("PlackettLuce")
+library("patchwork")
+library("qvcalc")
+library("ggparty")
+
+
+load("data/common_bean.rda")
+
+
+# compute the number of days required to accumulate gdd from 
+# planting date to maturity
+gdd <- GDD(modis, 
+           day.one = cbean$planting_date, 
+           degree.days = 900, 
+           return.as = "ndays")
+
+# add gdd to the cbean data
+# and take the average of gdd per season
+cbean %<>%  
+  mutate(gdd = gdd$gdd) %>% 
+  group_by(season) %>% 
+  mutate(gdds = as.integer(mean(gdd)))
+  
+# compute the temperature indices from planting date to the number of days 
+# required to accumulate the gdd in each season
+temp <- temperature(modis, 
+                    day.one = cbean$planting_date, 
+                    span = cbean$gdds)
+
+rain <- rainfall(chirps, 
+                    day.one = cbean$planting_date, 
+                    span = cbean$gdds)
+
+
+# round values to 3 decimals, this will not change the main result, but will 
+# help in visualizing the tree 
+temp[1:ncol(temp)] <- lapply(temp[1:ncol(temp)], function(x) round(x, 2))
+
+# combine the indices with the main data
+cbean <- cbind(cbean, temp, rain)
+
+# fit a PL tree
+plt <- pltree(G ~ maxNT + SU, data = cbean, minsize = 50)
+
+plot(plt)
+
+gosset:::plot_tree(plt)
