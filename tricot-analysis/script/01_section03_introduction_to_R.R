eb60dc69ae011d7f87204407cdfcc7092a26b63f
kauedesousa <kaue.desousa@inn.no>
2021-03-18 14:09:10 +0100

    5

--- tricot-analysis/script/01_section03_introduction_to_R.R
+++ tricot-analysis/script/01_section03_introduction_to_R.R
@@ -15,25 +15,36 @@
 # Numbers are native elements and are recognised by R without the 
 # need of quotation mark " "
 # an integer
-c(1, 2, 3) 
+c(1, 2, 3)
+
 
 # To assign strings (characters) to a vector you need to wrap 
 # each element with a quotation mark
 # a vector with characters (strings)
 c("red", "yellow", "green") 
 
+c("red", "yellow", "green")
+
+
+c(1, 2, 3)
+
 # a logical
 c(TRUE, FALSE, TRUE) 
 
+# something
 # R is a case sensitive language, 
 # TRUE is different than true or True 
 # true, TRUE, True
 
+
+
 # Missing values (of any type) are represented by logical constant `NA`.
 
-c("apple", "banana", "orange", NA)
+c("apple",  "banana", "orange", NA)
 
 length(c("apple", "banana", "orange", NA))
+
+
 length(c("apple", "banana", "orange"))
 
 # The function `is.na()` helps in finding the `NA`s in a vector
@@ -49,9 +60,11 @@ is.na(c("apple", "banana", "orange", NA))
 # Lists are the R objects which contain elements of different types like numbers, 
 # characters, vectors or even another list inside it
 
-list(c(1, 2, 3, 4, 5),
-     c("A", "B", "C", NA, "S"),
-     TRUE)
+list(
+  c(1, 4, 6, 7, 10, 11),
+  c("A", "G", "R", "J", NA),
+  FALSE
+)
 
 # ................................
 # ................................
@@ -61,14 +74,20 @@ list(c(1, 2, 3, 4, 5),
 #  a list of equal-length vectors and can be created with the function `data.frame()`
 
 iris
+
 str(iris)
+
+dim(iris)
+
+ncol(iris)
+
+nrow(iris)
 
 # creating a data frame
 
-data.frame(x = c(1:3),
-           y = c(3:5),
-           z = c("red", "blue", "black"),
-           w = c(TRUE, FALSE, TRUE))
+data.frame(farmername = c("Kjs", "Jsk", "Opa", "Ghs", "Lkd"),
+           age = c(23, 34, 50, 40, 36),
+           participated = c(TRUE, FALSE, TRUE, TRUE, TRUE))
 
 # ................................
 # ................................
@@ -76,18 +95,15 @@ data.frame(x = c(1:3),
 # R objects #####
 
 # objects are assigned with <- or =
-mydf <- data.frame(x = c(1:3),
-                   y = c(3:5),
-                   z = c("red", "blue", "black"),
-                   w = c(TRUE, FALSE, TRUE))
 
-# or
-x <- c(1:3)
-y <- c(3:5)
-z <- c("red", "blue", "black")
-w <- c(TRUE, FALSE, TRUE)
+dt <- data.frame(farmername = c("Kjs", "Jsk", "Opa", "Ghs", "Lkd"),
+                 age = c(23, 34, 50, 40, 36),
+                 participated = c(TRUE, FALSE, TRUE, TRUE, TRUE))
 
-mydf <- data.frame(x, y, z, w)
+
+farmsize <- c(0.3, 0.5, 1.2, 0.6, 2)
+
+dt <- cbind(dt, farmsize)
 
 
 # ................................
