2bd442bff867091da59da6ce6ba800623ad8cef2
kauedesousa <kaue.desousa@inn.no>
2021-05-03 10:56:57 +0200

    update-links-of-videos

--- tricot-analysis/script/05_plackettluce_trees.R
+++ tricot-analysis/script/05_plackettluce_trees.R
@@ -13,10 +13,17 @@ library("ClimMobTools")
 # ................................
 # ................................
 # Example 1: Sweetpotato data
+list.files("data")
+
 dt <- read.csv("data/sweet_potato.csv")
 
 # keep only the Ghana data
-dt <- dt[dt$country == "Ghana", ]
+keep <- dt$country == "Ghana"
+
+table(keep)
+
+dt <- dt[keep, ]
+
 
 head(dt)
 
@@ -26,21 +33,47 @@ R <- rank_tricot(data = dt,
                  input = c("best_overall","worst_overall"))
 
 
+
+head(R)
+
+?PlackettLuce
+
 mod <- PlackettLuce(R)
 
 summary(mod)
+
+unique(dt$item_A)
 
 ref = "SARI-Nyumingre (Obare)"
 
 summary(mod, ref = ref)
 
-plot(qvcalc(mod, ref = ref))
+qv <- qvcalc(mod, ref = ref)
+
+plot(qv)
 
 coef(mod, ref = ref)
 
-coef(mod, log = FALSE)
+mycoefs <- coef(mod, log = FALSE)
 
-sum(coef(mod, log = FALSE))
+sum(mycoefs)
+
+sort(mycoefs)
+
+
+capture.output(summary(mod),
+               coef(mod), 
+               file = "output/summary_mod_sweetpotato.txt")
+
+
+png(filename = "output/qvcalc_mod_sweetpotato.png",
+    width = 30,
+    height = 15,
+    units = "cm",
+    res = 400)
+plot(qv)
+dev.off()
+
 
 # ................................
 # ................................
@@ -49,10 +82,15 @@ names(dt)
 
 sel <- c("age", "gender")
 
-covar <- dt[sel]
+covar <- dt[, sel]
+
+str(covar)
+
 
 covar$age <- as.integer(covar$age)
 covar$gender <- as.factor(covar$gender)
+
+summary(covar$gender)
 
 # check for NAs
 sum(is.na(covar))
@@ -61,11 +99,13 @@ keep <- apply(covar, 1, function(x){
   sum(is.na(x))
 })
 
+keep
+
 keep <- keep == 0
 
 sum(keep)
 
-# filter the data
+# filter 
 covar <- covar[keep, ]
 dt <- dt[keep, ]
 
@@ -77,6 +117,8 @@ G <- rank_tricot(data = dt,
                  input = c("best_overall","worst_overall"),
                  group = TRUE)
 
+G
+
 # combine it with the covariates
 pldt <- cbind(G, covar)
 
@@ -85,9 +127,14 @@ head(pldt)
 str(pldt)
 
 # fit the PL tree
-pl <- pltree(G ~ age + gender, data = pldt, alpha = 0.1)
+names(pldt)
 
-summary(pl)
+pl <- pltree(G ~ age + gender, data = pldt, alpha = 0.1, 
+             minsize = 30,
+             verbose = TRUE)
+
+
+summary(pl, ref = ref)
 
 plot(pl)
 
@@ -106,16 +153,23 @@ dt <- getDataCM(key = key,
                 project = "breadwheat",
                 pivot.wider = TRUE)
 
+names(dt)
 
-names(dt) <- gsub("firstassessment_|package_|lastassessment_|registration_", "",
+names(dt) <- gsub("firstassessment_|package_|lastassessment_|registration_", 
+                  "",
                   names(dt))
 
 
+
 dt$plantingdate <- as.Date(dt$plantingdate, format = "%Y-%m-%d")
+
+boxplot(dt$plantingdate)
+
 dt$lon <- as.numeric(dt$farm_geo_longitude)
 dt$lat <- as.numeric(dt$farm_geo_latitude)
 
-temp <- temperature(dt[, c("lon","lat")], 
+
+temp <- temperature(dt[, c("lon", "lat")],
                     day.one = dt[, "plantingdate"],
                     span = 80)
 
@@ -129,11 +183,28 @@ G <- rank_tricot(dt,
 
 dat <- cbind(G, temp)
 
+head(dat)
+
 pl <- pltree(G ~ maxNT + maxDT, 
              data = dat)
 
 summary(pl)
 
 plot(pl)
+
+
+gosset:::plot_tree(pl)
+
+coef(pl)
+
+coef(pl, log = FALSE)
+
+
+predict(pl, newdata = dat[1:10, ])
+
+
+?predict
+
+
 
 
